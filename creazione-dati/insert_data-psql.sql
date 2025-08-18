f376e7cff72535a463241b0f0ca23930c993d881
simplefr4nk <simplefr4nk@proton.me>
2025-08-18 17:50:17 +0200

    fix(postgres): fix creazione di ingaggio e aggiunta del file per psql

---
+++ creazione-dati/insert_data-psql.sql
@@ -0,0 +1,13 @@
+\copy citta FROM 'citta.csv' WITH DELIMITER ',' CSV HEADER;
+\copy genere FROM 'genere.csv' WITH DELIMITER ',' CSV HEADER;
+\copy agenzia FROM 'agenzia.csv' WITH DELIMITER ',' CSV HEADER;
+\copy artista FROM 'artista.csv' WITH DELIMITER ',' CSV HEADER;
+\copy gruppo FROM 'gruppo.csv' WITH DELIMITER ',' CSV HEADER;
+\copy ambiente FROM 'ambiente.csv' WITH DELIMITER ',' CSV HEADER;
+\copy persona FROM 'persona.csv' WITH DELIMITER ',' CSV HEADER;
+\copy diritto_prevendita FROM 'diritto_prevendita.csv' WITH DELIMITER ',' CSV HEADER;
+\copy artista_in_gruppo FROM 'artista_in_gruppo.csv' WITH DELIMITER ',' CSV HEADER;
+\copy concerto FROM 'concerto.csv' WITH DELIMITER ',' CSV HEADER;
+\copy ingaggio_artista FROM 'ingaggio_artista.csv' WITH DELIMITER ',' CSV HEADER;
+\copy ingaggio_gruppo FROM 'ingaggio_gruppo.csv' WITH DELIMITER ',' CSV HEADER;
+\copy biglietto FROM 'biglietto.csv' WITH DELIMITER ',' CSV HEADER;
